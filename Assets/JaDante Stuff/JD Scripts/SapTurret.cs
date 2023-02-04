e1f08a89f181e7ca57bc6f4985f71878dfc0d9c7
swindle627 <jdpackers544@gmail.com>
2023-02-03 22:53:52 -0500

    Test Push

---
+++ Assets/JaDante Stuff/JD Scripts/SapTurret.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SapTurret : MonoBehaviour
+{
+    private void Start()
+    {
+        //towerManager = FindObjectOfType<TowerManager>();
+        //projectileSpawn = towerManager.sapSpawn;
+        //towerPrice = towerManager.sapPrice;
+    }
+    //public override void TowerAttack()
+    //{
+
+    //}
+}
---
+++ Assets/JaDante Stuff/JD Scripts/SeedTurret.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedTurret : MonoBehaviour
+{
+    
+    private void Start()
+    {
+        //towerManager = FindObjectOfType<TowerManager>();
+        //projectileSpawn = towerManager.seedSpawn;
+        //towerPrice = towerManager.seedPrice;
+    }
+    //public override void TowerAttack()
+    //{
+
+    //}
+}
