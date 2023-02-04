69890aabe4fdc18a9659924308f8772b9c3ed15a
swindle <jdpackers544@gmail.com>
2023-02-04 10:27:13 -0500

    Basic Tower Attack Code

--- Assets/JaDante Stuff/JD Scripts/SeedTurret.cs
+++ Assets/JaDante Stuff/JD Scripts/SeedTurret.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class SeedTurret : MonoBehaviour
 {
+    public Vector3 center;
+    public float attackRange;
     // change mesh for upgrade
     private void Start()
     {
-        //towerManager = FindObjectOfType<TowerManager>();
-        //projectileSpawn = towerManager.seedSpawn;
-        //towerPrice = towerManager.seedPrice;
-    }
-    //public override void TowerAttack()
-    //{
 
-    //}
+    }
+    private void Update()
+    {
+        Collider[] inRange = Physics.OverlapSphere(center, attackRange);
+
+        if (inRange.Length > 0)
+        {
+            Collider closest = FindClosest(inRange);
+            Attack(closest.gameObject);
+        }
+    }
+    private Collider FindClosest(Collider[] inRange)
+    {
+        Collider closest = inRange[0];
+        float lowest = Vector3.Distance(closest.gameObject.transform.position, gameObject.transform.position);
+
+        foreach(Collider collider in inRange)
+        {
+            float temp = Vector3.Distance(collider.gameObject.transform.position, gameObject.transform.position);
+            if(temp < lowest)
+            {
+                temp = lowest;
+                closest = collider;
+            }
+        }
+
+        return closest;
+    }
+    public void Attack(GameObject closest)
+    {
+        transform.LookAt(closest.transform.position);
+
+        // instantiate projectile
+    }
 }
